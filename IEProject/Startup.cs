f6556df68a60abc911e1be5ad616f20b260e3960
Xiao00c <93919xuxu@gmail.com>
2018-08-21 15:55:39 +1000

    Add project files.

---
+++ IEProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IEProject.Startup))]
+namespace IEProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
