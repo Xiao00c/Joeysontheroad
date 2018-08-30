41741776cc1de64e4e9149228a967b57c870df73
Echo <42601289+EchoZhao00@users.noreply.github.com>
2018-08-30 21:23:47 +1000

    Add files via upload

---
+++ frontEndWeb3.0/frontEndWeb3.0/question.js
@@ -0,0 +1,32 @@
+var questions = [{
+	"question":"for example the 1 qustion is......?",
+	"option1":"choose A",
+	"option2":"choose B",
+	"option3":"choose C",
+	"answer":"1"
+},{
+	"question":"for example the 2 qustion is......?",
+	"option1":"choose A",
+	"option2":"choose B",
+	"option3":"choose C",
+	"answer":"1"
+},{
+	"question":"for example the 3 qustion is......?",
+	"option1":"choose A",
+	"option2":"choose B",
+	"option3":"choose C",
+	"answer":"1"
+},{
+	"question":"for example the 4 qustion is......?",
+	"option1":"choose A",
+	"option2":"choose B",
+	"option3":"choose C",
+	"answer":"1"
+},{
+	"question":"for example the 5 qustion is......?",
+	"option1":"choose A",
+	"option2":"choose B",
+	"option3":"choose C",
+	"answer":"1"
+}
+];
